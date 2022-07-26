266fb8c8aa06e96a7bbaa13630ec7dadc174c23d
greedy <litdong@gmail.com>
2022-07-27 00:10:25 +0800

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -9,6 +9,4 @@ while true; do
     echo Deploy command failed, retrying in 5 secs...
     sleep 5
 done
-
-##exec gunicorn -b :5000 --access-logfile - --error-logfile - flasky:app
-flask run
+exec gunicorn -b :5000 --access-logfile - --error-logfile - flasky:app
